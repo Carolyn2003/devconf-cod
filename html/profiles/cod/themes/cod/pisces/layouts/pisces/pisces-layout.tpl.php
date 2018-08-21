a8839ea09aee1a293e1dca12efa8cc4b217c44e0
Carolyn <cdejesus@redhat.com>
2018-08-21 12:17:38 -0400

    Not printing site name

--- html/profiles/cod/themes/cod/pisces/layouts/pisces/pisces-layout.tpl.php
+++ html/profiles/cod/themes/cod/pisces/layouts/pisces/pisces-layout.tpl.php
@@ -6,7 +6,7 @@
           <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" class="site-branding__logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" /></a>
         <?php endif; ?>
         <?php if ($site_name): ?>
-          <a href="<?php print $front_page; ?>" class="site-branding__name" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
+          <a href="<?php print $front_page; ?>" class="site-branding__name" title="<?php print t('Home'); ?>" rel="home"><span><?php ?></span></a>
         <?php endif; ?>
         <?php if ($site_slogan): ?>
           <h2 class="site-branding__slogan"><?php print $site_slogan; ?></h2>
